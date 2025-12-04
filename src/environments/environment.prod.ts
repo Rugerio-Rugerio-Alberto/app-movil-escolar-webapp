0417a4f7a579f33ad432eafff92c327e4522dd30
Alberto Rugerio <alberto180310@gmail.com>
2025-12-04 02:27:23 -0600

    Fix environment urls for Render backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
-// src/environments/environment.prod.ts
 export const environment = {
   production: true,
+  // backend en Render
+  url_api: 'https://app-movil-escolar-api-r7sq.onrender.com',
   apiUrl: 'https://app-movil-escolar-api-r7sq.onrender.com'
 };
